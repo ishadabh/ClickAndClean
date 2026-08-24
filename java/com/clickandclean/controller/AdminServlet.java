f9cf572b7fd222440f47a33da5a41c7d7c2c610b
Shadab Hussain <shadabh255@gmail.com>
2026-08-25 03:25:31 +0530

    add admin servlet

---
+++ java/com/clickandclean/controller/AdminServlet.java
@@ -0,0 +1,54 @@
+package com.clickandclean.controller;
+
+import com.clickandclean.dao.ReportDAO;
+
+import com.clickandclean.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebServlet("/admin")
+public class AdminServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+	private ReportDAO reportDAO;
+
+    public void init() {
+        reportDAO = new ReportDAO();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect("index.jsp");
+            return;
+        }
+
+        User user = (User) session.getAttribute("user");
+        if (!"admin".equalsIgnoreCase(user.getRole())) {
+        	
+            response.sendError(HttpServletResponse.SC_FORBIDDEN );
+            return;
+        }
+        String action = request.getParameter("action");
+
+        if ("assign".equals(action)) {
+        	
+        	String temp = request.getParameter("reportId");
+            int reportId = Integer.parseInt(temp);
+            String driver = request.getParameter("driver");
+            reportDAO.assignReport(reportId, driver);
+        }
+
+        response.sendRedirect("dashboard");
+    }
+}
